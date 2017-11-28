21ae3866edb3beb0074b7885fdbf9136cc356dc0
Matthew Rizzini <matthewrizzini@gmail.com>
2017-11-28 04:02:02 +0000

    Displaying name, in-class work

---
+++ index.js
@@ -0,0 +1,38 @@
+/*global $ APIKEY*/
+$(document).ready (function() {
+    $.ajax({
+        method: "GET",
+        url: "https://newsapi.org/v2/sources",
+        data: { category:"technology", country: "us", language: "en", apiKey: APIKEY },
+        success: function(data) {
+            if (data.status == "ok") {
+                console.log(data);
+                for (var i = 0; i < data.sources.length; i++) {
+                    var source = document.createElement("OPTION");
+                    source.setAttribute("value", data.sources[i].name);
+                    source.innerHTML = data.sources[i].name;
+                    document.getElementById('selection').appendChild(source);
+                }
+            }
+        }
+    })
+//     .done(function( data ) {
+//         console.log( data );
+//         console.log( data.status );
+//         console.log( data.sources[0] );
+//   });
+     $('#source').submit(function(event) {
+        event.preventDefault();
+        alert(document.getElementById("selection").value);
+        // alert(document.getElementById("selection").name);
+        
+        for (var i = 0; i < document.getElementById("selection").length; i++);
+        var articleName = document.getElementById("selection").value;
+        var article = document.getElementById("article");
+        article.innerHTML = articleName;
+        
+    });
+});
+
+// https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=a8fddf92450a41a696d220a9cce08170
+
